a2acf9cd12fd1b5546bd4ecf69b7eee30866ff3e
Alessandro Caliciotti <acaliciotti@gmail.com>
2026-08-28 23:34:25 +0200

    fix(uart_tcp): configure SO_RCVTIMEO and proper errno handling for non-blocking lwIP socket

--- src/driver/drv_uart_tcp.c
+++ src/driver/drv_uart_tcp.c
@@ -80,17 +80,24 @@ void UART_TCP_TRX_Thread(void* arg)
 			client_sock = accept(listen_sock, (struct sockaddr*)&source_addr, &addr_len);
 			if(client_sock != INVALID_SOCK)
 			{
-				int c_flags = fcntl(client_sock, F_GETFL, 0);
-				fcntl(client_sock, F_SETFL, c_flags | O_NONBLOCK);
+				struct timeval tv;
+				tv.tv_sec = 0;
+				tv.tv_usec = 10000; // 10ms timeout
+				setsockopt(client_sock, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof(tv));
+				setsockopt(client_sock, SOL_SOCKET, SO_SNDTIMEO, (const char*)&tv, sizeof(tv));
+				int nodelay = 1;
+				setsockopt(client_sock, IPPROTO_TCP, TCP_NODELAY, (const char*)&nodelay, sizeof(nodelay));
+
 				if(g_conn_channel >= 0) CHANNEL_Set(g_conn_channel, 1, CHANNEL_SET_FLAG_SKIP_MQTT | CHANNEL_SET_FLAG_SILENT);
 				ADDLOG_INFO(LOG_FEATURE_DRV, "UART TCP Client connected!");
 			}
 		}
 
-		// 2. Bidirectional non-blocking forwarding
+		// 2. Bidirectional forwarding
 		if(client_sock != INVALID_SOCK)
 		{
 			// TCP -> UART
+			errno = 0;
 			int r = recv(client_sock, rx_buf, sizeof(rx_buf), 0);
 			if(r > 0)
 			{
@@ -99,12 +106,23 @@ void UART_TCP_TRX_Thread(void* arg)
 					UART_SendByte(rx_buf[i]);
 				}
 			}
-			else if(r == 0 || (r < 0 && errno != EAGAIN && errno != EWOULDBLOCK))
+			else if(r == 0)
 			{
-				ADDLOG_INFO(LOG_FEATURE_DRV, "UART TCP Client disconnected");
+				ADDLOG_INFO(LOG_FEATURE_DRV, "UART TCP Client disconnected gracefully");
 				close(client_sock);
 				client_sock = INVALID_SOCK;
 				if(g_conn_channel >= 0) CHANNEL_Set(g_conn_channel, 0, CHANNEL_SET_FLAG_SKIP_MQTT | CHANNEL_SET_FLAG_SILENT);
+			}
+			else
+			{
+				int err = errno;
+				if(err != 0 && err != EAGAIN && err != EWOULDBLOCK && err != EINTR)
+				{
+					ADDLOG_INFO(LOG_FEATURE_DRV, "UART TCP Client error %d", err);
+					close(client_sock);
+					client_sock = INVALID_SOCK;
+					if(g_conn_channel >= 0) CHANNEL_Set(g_conn_channel, 0, CHANNEL_SET_FLAG_SKIP_MQTT | CHANNEL_SET_FLAG_SILENT);
+				}
 			}
 
 			// UART -> TCP
@@ -119,12 +137,17 @@ void UART_TCP_TRX_Thread(void* arg)
 						tx_buf[i] = UART_GetByte(i);
 					}
 					UART_ConsumeBytes(ulen);
+					errno = 0;
 					int s_ret = send(client_sock, tx_buf, ulen, 0);
-					if(s_ret <= 0 && errno != EAGAIN && errno != EWOULDBLOCK)
+					if(s_ret < 0)
 					{
-						close(client_sock);
-						client_sock = INVALID_SOCK;
-						if(g_conn_channel >= 0) CHANNEL_Set(g_conn_channel, 0, CHANNEL_SET_FLAG_SKIP_MQTT | CHANNEL_SET_FLAG_SILENT);
+						int err = errno;
+						if(err != 0 && err != EAGAIN && err != EWOULDBLOCK && err != EINTR)
+						{
+							close(client_sock);
+							client_sock = INVALID_SOCK;
+							if(g_conn_channel >= 0) CHANNEL_Set(g_conn_channel, 0, CHANNEL_SET_FLAG_SKIP_MQTT | CHANNEL_SET_FLAG_SILENT);
+						}
 					}
 				}
 			}
